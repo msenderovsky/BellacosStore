e7393bec260b893a73e680a3e728f637b845edcf
msenderovsky <msenderovsky@gmail.com>
2021-12-23 17:01:21 -0300

    navBar con icono

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../../assets/logo.png'
 import Container from 'react-bootstrap/Container'
 import { Navbar, Nav } from 'react-bootstrap';
+import CartWidget from './CartWidget';
 
 const NavBar = () =>{
     return (
@@ -10,7 +11,7 @@ const NavBar = () =>{
                 <img src={logo} style={{paddingRight:'10px'}}></img>
                 <Navbar.Brand href="#inicio">Inicio</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="#shop">Shop</Nav.Link>
+                    <Nav.Link href="#shop"><CartWidget/></Nav.Link>
                     <Nav.Link href="#disenos">Diseños</Nav.Link>
                     <Nav.Link href="#contacto">Contacto</Nav.Link>
                 </Nav>
